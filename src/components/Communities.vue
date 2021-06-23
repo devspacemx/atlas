4c3346482477b2ca19065c179a54d2e86dd3750a
Alfonso Reyes <vaca.hace.muu@gmail.com>
2021-06-22 21:28:57 -0600

    feat: add communities in front page

--- src/components/Communities.vue
+++ src/components/Communities.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="py-3" id="comunitties">
+  <section class="py-3" id="communities">
     <div class="container">
       <div class="row flex-center">
         <div class="col-auto text-center my-4">
-          <h3 class="display-3 fw-bold">
+          <h5 class="display-5 fw-bold">
             Últimas comunidades<br />registradas
-          </h3>
+          </h5>
         </div>
       </div>
       <div class="row">
@@ -15,25 +15,17 @@
           :key="item.node.id"
           class="col-md-4 mb-5 mb-md-0"
         >
-          <div class="row d-flex align-items-center">
-            <div class="col-md-4 flex-shrink-0">
-              <g-image class="img-fluid shadow-sm" :src="item.node.image" />
-            </div>
-            <div class="col-md-7 flex-grow-1 ms-3">
-              <h5 class="fs-2 fs-lg-3 fw-bold">
-                {{ item.node.title }}
-              </h5>
-              <div class="icon"><span>i</span> Culiacán</div>
-              <div class="icon"><span>i</span> Culiacán</div>
-              <div class="icon"><span>i</span> Culiacán</div>
-            </div>
-          </div>
+          <img class="shadow-sm" :src="item.node.image" height="200" fit="contain" />
           <div class="mt-3 text-center text-md-start">
-            <h5>DESCRIPCIÓN</h5>
+            <h4 class="display-6 fs-2 fs-lg-3 fw-bold">
+              {{ item.node.title }}
+            </h4>
             <p class="mb-0">
               {{ item.node.description }}
             </p>
-            <a class="btn btn-link ps-0" href="#" role="button">a</a>
+            <a class="btn btn-link ps-0" href="#" role="button">
+              Ecommerce Wordpress ›</a
+            >
           </div>
         </g-link>
       </div>
@@ -42,7 +34,7 @@
 </template>
 <static-query>
 query Community {
-  communities: allCommunity(sortBy: "date") {
+  communities: allCommunity(sortBy: "date", limit: 6) {
     edges {
       node {
         id
@@ -56,6 +48,10 @@ query Community {
   }
 }
 </static-query>
+
+<style lang="scss" scoped>
+
+</style>
 <script>
-export default {};
+
 </script>
