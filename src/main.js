9992e048a4f3ea0f0f4707383d2492c14960a951
Alfonso Reyes <vaca.hace.muu@gmail.com>
2021-06-23 10:33:36 -0600

    feat: add fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Vue from "vue";
 import "./assets/styles/scss/theme.scss";
 import DefaultLayout from "./layouts/Default.vue";
 import checkIfMobile from "./mixins/checkIfMobile";
+
+library.add(faUserSecret);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = true;
 
 export default function(Vue, { router, head, isClient }) {
   Vue.component("Layout", DefaultLayout);
